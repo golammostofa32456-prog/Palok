4d6b7fee057581462cdaab22e242dabdb4b8e8c2
golammostofa32456-prog <golammostofa32456@gmail.com>
2026-09-11 14:22:58 +0600

    Update home_screen.dart

--- mobile/lib/screens/home_screen.dart
+++ mobile/lib/screens/home_screen.dart
@@ -1,24 +1,335 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'package:video_player/video_player.dart';
 
-void main() {
-  runApp(const PalokApp());
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class PalokApp extends StatelessWidget {
-  const PalokApp({super.key});
+class _HomeScreenState extends State<HomeScreen> {
+  final List<String> videoUrls = [
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4',
+  ];
+
+  final List<String> usernames = [
+    '@palok_user',
+    '@palok_creator',
+  ];
+
+  final PageController pageController = PageController();
+
+  int currentPage = 0;
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Palok',
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Colors.black,
-        useMaterial3: true,
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: PageView.builder(
+        controller: pageController,
+        scrollDirection: Axis.vertical,
+        itemCount: videoUrls.length,
+        onPageChanged: (index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        itemBuilder: (context, index) {
+          return PalokVideoCard(
+            videoUrl: videoUrls[index],
+            username: usernames[index],
+            isActive: index == currentPage,
+          );
+        },
       ),
-      home: const HomeScreen(),
+    );
+  }
+}
+
+class PalokVideoCard extends StatefulWidget {
+  final String videoUrl;
+  final String username;
+  final bool isActive;
+
+  const PalokVideoCard({
+    super.key,
+    required this.videoUrl,
+    required this.username,
+    required this.isActive,
+  });
+
+  @override
+  State<PalokVideoCard> createState() => _PalokVideoCardState();
+}
+
+class _PalokVideoCardState extends State<PalokVideoCard> {
+  late VideoPlayerController controller;
+
+  bool isLiked = false;
+  bool isFollowing = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = VideoPlayerController.networkUrl(
+      Uri.parse(widget.videoUrl),
+    )..initialize().then((_) {
+        if (mounted) {
+          setState(() {});
+          controller.setLooping(true);
+          controller.play();
+        }
+      });
+  }
+
+  @override
+  void didUpdateWidget(covariant PalokVideoCard oldWidget) {
+    super.didUpdateWidget(oldWidget);
+
+    if (widget.isActive) {
+      controller.play();
+    } else {
+      controller.pause();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  void toggleLike() {
+    setState(() {
+      isLiked = !isLiked;
+    });
+  }
+
+  void toggleFollow() {
+    setState(() {
+      isFollowing = !isFollowing;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (controller.value.isPlaying) {
+          controller.pause();
+        } else {
+          controller.play();
+        }
+        setState(() {});
+      },
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Container(color: Colors.black),
+
+          if (controller.value.isInitialized)
+            FittedBox(
+              fit: BoxFit.cover,
+              child: SizedBox(
+                width: controller.value.size.width,
+                height: controller.value.size.height,
+                child: VideoPlayer(controller),
+              ),
+            )
+          else
+            const Center(
+              child: CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ),
+
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black54,
+                  Colors.transparent,
+                  Colors.black87,
+                ],
+              ),
+            ),
+          ),
+
+          SafeArea(
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const Text(
+                  'PALOK',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 4,
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            right: 12,
+            bottom: 125,
+            child: Column(
+              children: [
+                _profileButton(),
+                const SizedBox(height: 24),
+                _actionButton(
+                  icon: isLiked
+                      ? Icons.favorite
+                      : Icons.favorite_border,
+                  label: 'Like',
+                  color: isLiked ? Colors.red : Colors.white,
+                  onTap: toggleLike,
+                ),
+                const SizedBox(height: 24),
+                _actionButton(
+                  icon: Icons.comment_outlined,
+                  label: 'Comment',
+                  onTap: () {},
+                ),
+                const SizedBox(height: 24),
+                _actionButton(
+                  icon: Icons.share_outlined,
+                  label: 'Share',
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+
+          Positioned(
+            left: 16,
+            right: 80,
+            bottom: 35,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      widget.username,
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    GestureDetector(
+                      onTap: toggleFollow,
+                      child: Text(
+                        isFollowing ? 'Following' : 'Follow',
+                        style: TextStyle(
+                          color: isFollowing
+                              ? Colors.white70
+                              : Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  'Welcome to PALOK 🎬',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 15,
+                  ),
+                ),
+                const SizedBox(height: 8),
+                const Text(
+                  '#Palok #ShortVideo #Bangladesh',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _profileButton() {
+    return GestureDetector(
+      onTap: toggleFollow,
+      child: Column(
+        children: [
+          Container(
+            width: 52,
+            height: 52,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(
+                color: Colors.white,
+                width: 2,
+              ),
+              color: Colors.black54,
+            ),
+            child: const Icon(
+              Icons.person,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+          const SizedBox(height: 5),
+          Text(
+            isFollowing ? 'Following' : 'Follow',
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 11,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _actionButton({
+    required IconData icon,
+    required String label,
+    required VoidCallback onTap,
+    Color color = Colors.white,
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Column(
+        children: [
+          Icon(
+            icon,
+            color: color,
+            size: 34,
+          ),
+          const SizedBox(height: 4),
+          Text(
+            label,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 11,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
