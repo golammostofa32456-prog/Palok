2bb6165ca15e4effa9010e74a2edd21a062758c1
golammostofa32456-prog <golammostofa32456@gmail.com>
2026-09-11 22:50:46 +0600

    Update settings.gradle.kts

--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -4,7 +4,9 @@ pluginManagement {
             val properties = java.util.Properties()
             file("local.properties").inputStream().use { properties.load(it) }
             val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
+            require(flutterSdkPath != null) {
+                "flutter.sdk not set in local.properties"
+            }
             flutterSdkPath
         }
 
@@ -21,6 +23,8 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.1.0" apply false
     id("org.jetbrains.kotlin.android") version "2.4.0" apply false
+
+    id("com.google.gms.google-services") version "4.5.0" apply false
 }
 
 include(":app")
