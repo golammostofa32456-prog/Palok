a9ffe3745a1ba7bcf9af98187ad8c2cce4814489
golammostofa32456-prog <golammostofa32456@gmail.com>
2026-09-16 13:47:05 +0600

    Update main.dart

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'edit_profile_screen.dart';
 import 'login_screen.dart';
 import 'home_screen.dart';
 import 'splash_screen.dart';
