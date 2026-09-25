11c76dca80eedf4d3313bef77f1d1977478a52fc
golammostofa32456-prog <golammostofa32456@gmail.com>
2026-09-25 23:42:26 +0600

    Update video_player_widget.dart

--- mobile/lib/features/video/video_player_widget.dart
+++ mobile/lib/features/video/video_player_widget.dart
@@ -1,1 +1,41 @@
 
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+
+class VideoPlayerWidget extends StatelessWidget {
+  final VideoPlayerController controller;
+  final BoxFit fit;
+  final bool showLoading;
+
+  const VideoPlayerWidget({
+    super.key,
+    required this.controller,
+    this.fit = BoxFit.cover,
+    this.showLoading = true,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      if (!showLoading) {
+        return const SizedBox.expand();
+      }
+
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    return SizedBox.expand(
+      child: FittedBox(
+        fit: fit,
+        clipBehavior: Clip.hardEdge,
+        child: SizedBox(
+          width: controller.value.size.width,
+          height: controller.value.size.height,
+          child: VideoPlayer(controller),
+        ),
+      ),
+    );
+  }
+}
